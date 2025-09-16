c838798f1f897c485e710361336b10b367de7b2f
kkth3150 <kkth3150@naver.com>
2025-09-16 15:17:17 +0900

    네모

--- character_grass.py
+++ character_grass.py
@@ -6,6 +6,7 @@ open_canvas(800, 600)
 def PlayerMove():
     while True:
         Circle_Move()
+        Square_Move()
 
 
 def Circle_Move():
@@ -21,7 +22,19 @@ def Circle_Move():
         character.draw_now(x, y)
         delay(0.01)
 
-
+def Square_Move():
+    points = [(400, 80), (750, 80), (750, 550), (50, 550), (50, 80), (400, 80)]
+    for i in range(len(points) - 1):
+        x1, y1 = points[i]
+        x2, y2 = points[i + 1]
+        move = 100
+        dx = (x2 - x1) / move
+        dy = (y2 - y1) / move
+        for step in range(move):
+            clear_canvas_now()
+            grass.draw_now(400, 30)
+            character.draw_now(x1 + dx * step, y1 + dy * step)
+            delay(0.01)
 
 
 grass = load_image('grass.png')
